ab091f5542e73ebca6609dd31bbe6298a06f4bf7
Ivan Mwesigwa <86493684+ivanmwes2016@users.noreply.github.com>
2022-06-14 13:35:31 +0100

    added products

--- web/components/product.tsx
+++ web/components/product.tsx
@@ -8,7 +8,13 @@ const Product = ({product:{image, name, slug, price}}:any) => {
         <div>
             <Link href={`/product/${slug.current}`}>
                 <div className="product-card">
-                    <img src={urlFor(image && image[0]).toString()} alt=""/>
+                    <img src={urlFor(image && image[0]).toString()}
+                    width= {250}
+                    height={250}
+                    className="product-image"/>
+                    <div className="product-name">{name}</div>
+                    <div className="product-price">${price}</div>
+                    
                 </div>
             </Link>
         </div>
